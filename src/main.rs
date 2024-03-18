9d6939ed8b6a340278d5a5a6420022c63c49dae2
RaphaelKlein <raphael.klein@tum.de>
2024-03-18 04:53:48 +0100

    panic handler

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+
+#![no_std]
+
+//basically import of PanicInfo
+use core::panic::PanicInfo;
+
+//#[] are attributes that are used to mark stuff for the current crate
+#[panic_handler]
+//_ is convention because info parameter is not used
+//-> ! specifies return type of function. ! is the "never type" indicating returning NOTHING, not even control to the caller
+// (different to "void" equivalent of omitting the return type
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
 }
+
+fn main() {}
