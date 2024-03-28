ffb484b4669fa874b64d413e815f7bdd06ddfcbf
raphaelklein <raphael.klein@tum.de>
2024-03-29 00:01:24 +0100

    division by zero exception handler implementation done

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -8,8 +8,9 @@ lazy_static! {
     static ref IDT: InterruptDescriptorTable = {
         // initiating the ID Table for our exceptions
         let mut idt = InterruptDescriptorTable::new();
-        // mapping out breakpoint handler fn to the breakpoint exception to our idt
+        // mapping of handler fn to the exception of our idt
         idt.breakpoint.set_handler_fn(breakpoint_handler);
+        idt.divide_error.set_handler_fn(divided_by_zero_handler);
         // load the idt with lidt using the x86 crate method
         idt
     };
@@ -23,4 +24,9 @@ pub fn init_idt() {
 extern "x86-interrupt" fn breakpoint_handler(stack_frame: InterruptStackFrame) {
     // instead of temp pausing our program, we just print this message
     println!("BREAKPOINT EXCEPTION: \n{:#?}", stack_frame);
+}
+
+extern "x86-interrupt" fn divided_by_zero_handler(stack_frame: InterruptStackFrame) {
+    //print message if division by zero
+    println!("DIV BY ZERO EXCEPTION: \n{:#?}", stack_frame);
 }
